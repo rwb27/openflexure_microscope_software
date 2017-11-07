372346912509e2489fd250e30f88d6697cc97da6
Richard Bowman <richard.bowman@cantab.net>
2017-11-07 18:05:20 +0000

    Added setup.py
    should help this become a proper python package (eventually)

---
+++ setup.py
@@ -0,0 +1,13 @@
+__author__ = 'Richard Bowman
+
+from setuptools import setup, find_packages
+
+setup(name='openflexure_microscope
+      version='0.1'
+      description='Control scripts for the OpenFlexure Microscope',
+      url='http://www.github.com/rwb27/openflexure_microscope',
+      author='Richard Bowman',
+      author_email='r.w.bowman@bath.ac.uk',
+      license='GNU GPL v3',
+      packages=find_packages(),
+      zip_safe=True)
