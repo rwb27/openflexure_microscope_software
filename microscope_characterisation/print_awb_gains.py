7820a58c6a6d0c60d877251c17a91a1e12917bae
Richard Bowman <richard.bowman@cantab.net>
2017-11-22 18:00:39 +0000

    Added a script to print the AWB gains

---
+++ microscope_characterisation/print_awb_gains.py
@@ -0,0 +1,14 @@
+"""
+A script to print the current AWB gains of the Pi Camera
+"""
+import picamera
+
+
+if __name__ == "__main__":
+    with picamera.PiCamera(resolution=microscope.picam2_full_res) as camera:
+        camera.start_preview()
+        
+        time.sleep(3)
+		print "AWB Gains:"
+        print camera.awb_gains
+
